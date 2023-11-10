87fc6c1f7e62ef120bbb9124873f2780b3dbc523
justbustin <justinnguyen80@gmail.com>
2023-11-10 08:15:02 -0800

    add cors

--- express-server/app.js
+++ express-server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { exec } = require('child_process');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
