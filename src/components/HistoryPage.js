5c92416a8234154add99a316324e9fdb9b42c203
jordan <jyen@jordans-macbook-pro.local>
2023-11-17 06:32:23 -0800

    data

--- src/components/HistoryPage.js
+++ src/components/HistoryPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import About from './About';
 import HistoricGraph from './HistoricGraph';
+import SeedGraph from './SeedGraph';
 import Graph from './Graph';
 
 function HistoryPage() {
@@ -19,6 +20,13 @@ function HistoryPage() {
                 <h3>Daily Summary</h3>
             <Graph defaultData={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]} defaultLabels={['Start', '9:43:18AM', '9:43:38AM', '9:44:18AM', '9:44:38AM', '9:45:18AM', '9:45:38AM', '9:46:18AM', '9:46:38AM']}/>
 
+            </div>
+        </div>
+
+        <div className="d-flex flex-column gap-5">
+            <div className='d-flex justify-content-center align-items-center gap-5'>
+                <h3>Seed Graph</h3>
+                <SeedGraph />
             </div>
         </div>
         
