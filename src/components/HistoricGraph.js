4099055cdcf309ff98ff6c304ac743a2f618ed31
justbustin <justinnguyen80@gmail.com>
2023-11-17 18:32:57 -0800

    historic graph

--- src/components/HistoricGraph.js
+++ src/components/HistoricGraph.js
@@ -1,68 +1,79 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Chart from 'chart.js/auto';
+import React, { useEffect, useRef, useState } from "react";
+import Chart from "chart.js/auto";
 
-
-function HistoricGraph() {
-
-
-const chartRef = useRef(null);
+function HistoricGraph({ data, labels }) {
+  const chartRef = useRef(null);
   const [myChart, setMyChart] = useState(null);
 
   useEffect(() => {
-    if (chartRef.current) {
-
-      const myChart = new Chart(chartRef.current, {
-        type: 'bar',
-        data: {
-          labels: ['11/3', '11/4', '11/5', '11/6', '11/7', '11/8', '11/9', '11/10'],
-          datasets: [{
-            label: 'Week 11/3 - 11/10',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 205, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(201, 203, 207, 0.2)'
+    const createChart = () => {
+      if (chartRef.current) {
+        const newChart = new Chart(chartRef.current, {
+          type: "bar",
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                label: `Week ${labels[0]} - ${labels[labels.length - 1]}`,
+                data: data,
+                backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(255, 159, 64, 0.2)",
+                  "rgba(255, 205, 86, 0.2)",
+                  "rgba(75, 192, 192, 0.2)",
+                  "rgba(54, 162, 235, 0.2)",
+                  "rgba(153, 102, 255, 0.2)",
+                  "rgba(201, 203, 207, 0.2)",
+                ],
+                borderColor: [
+                  "rgb(255, 99, 132)",
+                  "rgb(255, 159, 64)",
+                  "rgb(255, 205, 86)",
+                  "rgb(75, 192, 192)",
+                  "rgb(54, 162, 235)",
+                  "rgb(153, 102, 255)",
+                  "rgb(201, 203, 207)",
+                ],
+                borderWidth: 1,
+              },
             ],
-            borderColor: [
-              'rgb(255, 99, 132)',
-              'rgb(255, 159, 64)',
-              'rgb(255, 205, 86)',
-              'rgb(75, 192, 192)',
-              'rgb(54, 162, 235)',
-              'rgb(153, 102, 255)',
-              'rgb(201, 203, 207)'
-            ],
-            borderWidth: 1}]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+            responsive: true,
+            maintainAspectRatio: true,
+          },
+        });
+        setMyChart(newChart);
+        return newChart;
+      }
+    };
 
-      setMyChart(myChart);
-
-
-      // Optional: Cleanup function in case of component unmounting
-      return () => {
-        myChart.destroy();
-      };
+    // Destroy the old chart if it exists
+    if (myChart) {
+      myChart.destroy();
     }
-  }, []);
 
+    // Create a new chart
+    const newChart = createChart();
 
-    return(
-        <div className="w-50">
-            <canvas ref={chartRef} className=""></canvas>
-      </div>
-    );
+    // Cleanup function for component unmount
+    return () => {
+      if (newChart) {
+        newChart.destroy();
+      }
+    };
+  }, [data, labels]); // Re-run effect if data or labels change
+
+  return (
+    <div className="w-50">
+      <canvas ref={chartRef}></canvas>
+    </div>
+  );
 }
 
 export default HistoricGraph;
--- src/components/HistoryPage.js
+++ src/components/HistoryPage.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
-import Header from './Header';
-import About from './About';
-import HistoricGraph from './HistoricGraph';
-import Graph from './Graph';
+import Header from "./Header";
+import HistoricGraph from "./HistoricGraph";
+import Graph from "./Graph";
 import { db } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { Dropdown } from "react-bootstrap";
 
 function HistoryPage() {
   const [dailyData, setDailyData] = useState([]);
   const [dailyLabels, setDailyLabels] = useState([]);
+  const [weeklyData, setWeeklyData] = useState();
+  const [weeklyLabels, setWeeklyLabels] = useState([]);
   const [chartKey, setChartKey] = useState(0);
+  const [currentWeek, setCurrentWeek] = useState("11/12 - 11/18");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,32 +25,107 @@ function HistoryPage() {
       const docRef = doc(db, "seeds", `${month}-${day}-${year}`);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
-        console.log("Document data: ", docSnap.data());
         setDailyData(docSnap.data().seeds);
 
-        const formattedLabels = docSnap.data().times.map((timestamp) => {
-          // Convert Unix timestamp to Date object
-          const date = new Date(timestamp * 1000); // Multiply by 1000 because Date() requires milliseconds
-          // Format the time
-          return date.toLocaleTimeString("en-US", {
-            hour: "numeric",
-            minute: "2-digit",
-            second: "2-digit",
-            hour12: true,
-          });
-        });
+        console.log(docSnap.data().times);
+        // const formattedLabels = docSnap.data().times.map((timestamp) => {
+        //   // Convert Unix timestamp to Date object
+        //   const date = new Date(timestamp * 1000); // Multiply by 1000 because Date() requires milliseconds
+        //   // Format the time
+        //   return date.toLocaleTimeString("en-US", {
+        //     hour: "numeric",
+        //     minute: "2-digit",
+        //     second: "2-digit",
+        //     hour12: true,
+        //   });
+        // });
 
-        setDailyLabels(formattedLabels);
+        setDailyLabels(docSnap.data().times);
         setChartKey((prevKey) => prevKey + 1);
+        console.log(dailyLabels);
+        console.log(dailyData);
       } else {
         console.log("No document");
       }
     };
+
+    const fetchWeekData = async () => {
+      const { weekData, weekLabels } = await fetchWeeklyData(
+        new Date(2023, 10, 12),
+        new Date(2023, 10, 18)
+      );
+      if (weekData && weekLabels) {
+        setWeeklyData(weekData);
+        setWeeklyLabels(weekLabels);
+      }
+    };
+
+    fetchWeekData();
     fetchData();
   }, []);
 
-  console.log(dailyData);
-  console.log(dailyLabels);
+  const fetchWeeklyData = async (start, end) => {
+    const weekData = [];
+    const weekLabels = [];
+
+    // Loop through each day of the week
+    for (let d = new Date(start); d <= end; d.setDate(d.getDate() + 1)) {
+      let day = d.getDate();
+      let month = d.getMonth() + 1; // getMonth() is zero-based
+      let year = d.getFullYear().toString().slice(-2);
+
+      // Format the date to match how it's stored in Firebase
+      let formattedDate = `${month}-${day}-${year}`;
+      let label = `${month}/${day}`;
+      weekLabels.push(label);
+      // Fetch data for the day
+      const docRef = doc(db, "seeds", formattedDate);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        weekData.push(docSnap.data().seeds[docSnap.data().seeds.length - 1]);
+      } else {
+        weekData.push(0);
+      }
+    }
+    return { weekData, weekLabels };
+  };
+
+  const handleDropdownItemClick = async (item) => {
+    let startDate, endDate;
+
+    // Determine the date range based on the clicked item
+    switch (item) {
+      case "action-1":
+        startDate = new Date(2023, 9, 29); // 10/29/2023
+        endDate = new Date(2023, 10, 4); // 11/4/2023
+        break;
+      case "action-2":
+        startDate = new Date(2023, 10, 5); // 11/5/2023
+        endDate = new Date(2023, 10, 11); // 11/11/2023
+        break;
+      case "action-3":
+        startDate = new Date(2023, 10, 12); // 11/12/2023
+        endDate = new Date(2023, 10, 18); // 11/18/2023
+        break;
+      default:
+        break;
+    }
+    let startMonth = startDate.getMonth() + 1; // getMonth() is zero-based
+    let startDay = startDate.getDate();
+
+    let endMonth = endDate.getMonth() + 1; // getMonth() is zero-based
+    let endDay = endDate.getDate();
+
+    setCurrentWeek(`${startMonth}/${startDay} - ${endMonth}/${endDay}`);
+
+    const { weekData, weekLabels } = await fetchWeeklyData(startDate, endDate);
+    if (weekData && weekLabels) {
+      setWeeklyData(weekData);
+      setWeeklyLabels(weekLabels);
+    }
+
+    // Now you can set your state with this weekly data
+  };
 
   return (
     <div>
@@ -55,10 +133,39 @@ function HistoryPage() {
       <h1 className="m-5">Historic Data</h1>
 
       <div className="d-flex flex-column gap-5">
-        <div className="d-flex justify-content-center align-items-center gap-5">
-          <h3>Weekly Summary</h3>
-          <HistoricGraph />
+        <div className="d-flex flex-column gap-5 justify-content-center align-items-center">
+          <div className="d-flex justify-content-center align-items-center gap-5">
+            <h3>Weekly Summary</h3>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                {currentWeek}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  href="#/action-1"
+                  onClick={() => handleDropdownItemClick("action-1")}
+                >
+                  10/29 - 11/4
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-2"
+                  onClick={() => handleDropdownItemClick("action-2")}
+                >
+                  11/5 - 11/11
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#/action-3"
+                  onClick={() => handleDropdownItemClick("action-3")}
+                >
+                  11/12 - 11/18
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          <HistoricGraph data={weeklyData} labels={weeklyLabels} />
         </div>
+
         <div className="d-flex justify-content-center align-items-center gap-5">
           <h3>Daily Summary</h3>
           <Graph
